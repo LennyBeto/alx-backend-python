1b1e5c0711933fc21293f1c7edcfe80ba65ed570
Lenny Beto <43434470+LennyBeto@users.noreply.github.com>
2025-07-13 21:14:20 +0300

    File added

--- python-decorators-0x01/1-with_db_connection.py
+++ python-decorators-0x01/1-with_db_connection.py
@@ -1,18 +1,19 @@
 import sqlite3
 import functools
 
+# Decorator to manage DB connection and pass it to the function
 def with_db_connection(func):
-    """Decorator to handle database connection."""
     @functools.wraps(func)
     def wrapper(*args, **kwargs):
-        # Open a database connection
-        conn = sqlite3.connect('users.db')
+        # Open DB connection
+        conn = sqlite3.connect("users.db")
         try:
-            # Call the original function with the connection
-            return func(conn, *args, **kwargs)
+            # Inject connection into the function
+            result = func(conn, *args, **kwargs)
         finally:
-            # Ensure the connection is closed after the function call
+            # Always close connection
             conn.close()
+        return result
     return wrapper
 
 @with_db_connection
@@ -21,6 +22,6 @@ def get_user_by_id(conn, user_id):
     cursor.execute("SELECT * FROM users WHERE id = ?", (user_id,))
     return cursor.fetchone()
 
-#### Fetch user by ID with automatic connection handling
+# Fetch user by ID with automatic connection handling
 user = get_user_by_id(user_id=1)
 print(user)
